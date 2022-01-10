bc32a68b65bbfe7fe2d0e1c3eb142031e0494b7e
Nikolas Caldeira <62756507+NikolasCaldeira@users.noreply.github.com>
2022-01-10 18:55:51 -0300

    Add files via upload

---
+++ Academia/src/Aluno.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nikol
+ */
+public class Aluno {
+    private String nome;
+    private int idade;
+    private float peso, altura;
+    private String objetivo;
+            
+    public Aluno(String nome, int idade, float peso, float altura, String objetivo){
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.objetivo = objetivo;
+    }
+}
